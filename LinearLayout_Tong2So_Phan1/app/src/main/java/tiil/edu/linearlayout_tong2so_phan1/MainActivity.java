0f70dc0ab6d2885892cb2e52e14a6fcb415cfc1d
ntnhan2111 <nhan.nth.63cntt@ntu.edu.vn>
2026-03-10 18:35:47 +0700

    Nộp bài TH2 _ Phần 1

---
+++ LinearLayout_Tong2So_Phan1/app/src/main/java/tiil/edu/linearlayout_tong2so_phan1/MainActivity.java
@@ -0,0 +1,14 @@
+package tiil.edu.linearlayout_tong2so_phan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
